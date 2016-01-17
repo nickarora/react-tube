3fe0546020f6958be12b582c937286745ec1750c
Nick Arora <arora.nick@gmail.com>
2016-01-17 07:43:32 -0800

    stubout search bar component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = "AIzaSyCj7QNZ5LBYOM-ahQkDzjnCVoU6S3zxqsU";
 
-// Create a new component.
-// This component should produce some HTML.
+
 const App = () => {
-    return <div>Hello World.</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-// Take this component's generated HTML and put
-// it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
 
