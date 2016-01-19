16ef31bc475370678034411a161063ea82da5adb
Nick Arora <arora.nick@gmail.com>
2016-01-19 11:33:56 -0800

    select videos from the video list using a callback

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
+const VideoList = ({onVideoSelect, videos}) => {
 
-    const videoItems = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video} />;
+    const videoItems = videos.map((video) => {
+        return <VideoListItem
+            key={video.etag}
+            onVideoSelect={onVideoSelect}
+            video={video} />;
     });
 
     return (
